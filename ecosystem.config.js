1f9178ee8bf368f95506d7adc388be3b399335f4
patrick <patrick.demarta@gmail.com>
2025-11-25 18:28:04 +0100

    new port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 4000
     },
     error_file: './logs/err.log',
     out_file: './logs/out.log',
